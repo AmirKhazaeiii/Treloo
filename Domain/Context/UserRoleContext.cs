a2f5a485144717e2f581d5243b8aca6f26aaf59a
pouria_khazaei <porya.khazaee.1374@gmail.com>
2024-02-09 00:15:52 +0330

    firstcommit

---
+++ Domain/Context/UserRoleContext.cs
@@ -0,0 +1,22 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Context
+{
+    public class UserRoleContext
+    {
+        private IRoleService iroleService;
+        public UserRoleContext(IRoleService roleService)
+        {
+            this.iroleService = roleService;
+        }
+        public string getRoleName()
+        {
+            return iroleService.getRoleName();
+        }
+    }
+}
---
+++ Domain/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Context;
+using Service.Interfaces;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string email { get; set; }
+        public UserRoleContext context { get; set; }
+
+        public User(IRoleService role)
+        {
+            context = new UserRoleContext(role);
+        }
+        public void showInfo()
+        {
+            Console.WriteLine($"name: {fullName}, role: {context.getRoleName()}");
+        }
+    }
+}
